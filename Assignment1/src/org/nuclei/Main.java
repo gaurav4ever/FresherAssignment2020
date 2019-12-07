0452cb7a18202884601b5c7631a0e65060b7ceea
manasjain0405 <manasjain111@gmail.com>
2019-12-08 03:10:37 +0530

    Removed old files

--- Assignment1/src/org/nuclei/Main.java
+++
@@ -1,9 +0,0 @@
-package org.nuclei;
-
-import org.nuclei.util.ItemUtility;
-
-    public class Main extends ItemUtility {
-        public static void main(String args[]) {
-            run();
-    }
-}
