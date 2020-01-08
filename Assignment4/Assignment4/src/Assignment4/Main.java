d1be2eab0128b93f8eb9f70f9396aeaad8c0feb4
Sudiksha Srivastava <sudikshasrivastava@Sudikshas-MBP.local>
2020-01-08 12:23:00 +0530

    Thread created

---
+++ Assignment4/Assignment4/src/Assignment4/Main.java
@@ -0,0 +1,48 @@
+package Assignment4;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException {
+
+        connectionClass conn = new connectionClass();
+        Operations operate = new Operations();
+        conn.create();
+
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    operate.select();
+                } catch (InterruptedException e) {
+                    System.out.println("Exception while Reading from database");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    operate.CalculateTaxAndUpdatePrice();
+                } catch (InterruptedException e) {
+                    System.out.println("Exception while Reading from database");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Item\tPrice\tTax\tFinal Price");
+
+        for(Items i1: operate.finalPricedItems)
+        {
+            System.out.println(i1.getName() + "\t" + i1.getPrice()+ "\t" + i1.getTax() + "\t" + i1.getFinalPrice());
+
+        }
+
+    }
+}
