f9b05e0d4cb4a720dec7cf296e73fbebbb5f0b8b
Piyush Mehta <me@piyushmehta.com>
2020-01-18 17:11:11 +0530

    output util

--- Q1/src/assignmentQ1/util/outputUtil.java
+++ Q1/src/assignmentQ1/util/outputUtil.java
@@ -1,4 +1,14 @@
 package assignmentQ1.util;
 
+import assignmentQ1.model.Item;
+
 public class outputUtil {
+//    output Util
+    public static void printOutput(Item productListItems, double salesTax, double totalPrice) {
+        System.out.println("Output details");
+        System.out.println("Product Name:"+ productListItems.productName);
+        System.out.println("Product Price:"+ productListItems.productPrice);
+        System.out.println("Product Name:"+ salesTax);
+        System.out.println("Product Name:"+ totalPrice);
+    }
 }
