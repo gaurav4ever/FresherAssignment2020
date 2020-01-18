7923e10166fb9b13d82a44ded91e0e3dba9224d8
Piyush Mehta <me@piyushmehta.com>
2020-01-18 19:16:43 +0530

    first done without test

--- Q1/src/assignmentQ1/Inventory.java
+++ Q1/src/assignmentQ1/Inventory.java
@@ -2,22 +2,36 @@ package assignmentQ1;
 
 import java.util.ArrayList;
 import assignmentQ1.model.Item;
+import assignmentQ1.services.CalculateTaxService;
+import assignmentQ1.services.Tax;
+import assignmentQ1.util.InputUtil;
+import assignmentQ1.util.OutputUtil;
+
 public class Inventory {
-    private static ArrayList<Item> arrayListItems = new ArrayList<Item>();
+    private static ArrayList<Item> productList = new ArrayList<Item>();
 
     public static void main(String[] args) {
-
+       try {
+           productList = InputUtil.getInputItem();
+           calculateTax();
+       } catch (Exception e) {
+            //prints exception which is caught
+            System.out.println(e.getMessage());
+        }
     }
 //    to calc tax of all items
     private static void calculateTax() {
-        for (Item arrayListItem: arrayListItems) {
-            double salesTax = getCalculatedTax(arrayListItem.type, arrayListItem.price);
+        for (Item arrayListItem : productList) {
+            double salesTax = getCalculatedTax(arrayListItem.productType, arrayListItem.productPrice);
+            double totalPrice = (arrayListItem.productPrice + salesTax) * arrayListItem.productQuantity;
+            OutputUtil.printOutput(arrayListItem, salesTax, totalPrice);
         }
     }
 
 //    select rule
     private static double getCalculatedTax(int type, double price){
-       return 0;
+        CalculateTaxService calculateTaxService = new Tax().getTax(type);
+        return calculateTaxService.calculateTax(price);
     }
 }
 
--- Q1/src/assignmentQ1/services/Tax.java
+++ Q1/src/assignmentQ1/services/Tax.java
@@ -4,8 +4,15 @@ public class Tax {
     public CalculateTaxService  getTax(int type) {
     switch(type) {
         case 1:
-            return 0;
-            case
+            return new RawTax();
+        case 2:
+            return new ManufacturedTax();
+        case 3:
+            return new ImportedTax();
+        default:
+            System.out.println("You have entered a wrong choice!");
+            System.exit(0);
     }
+    return null;
     }
 }
