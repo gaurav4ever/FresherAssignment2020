291552c28a2340880508e9d3e746fa09cc626a12
Piyush Mehta <me@piyushmehta.com>
2020-01-18 18:52:54 +0530

    taxes

--- Q1/src/assignmentQ1/services/ManufacturedTax.java
+++ Q1/src/assignmentQ1/services/ManufacturedTax.java
@@ -1,4 +1,10 @@
 package assignmentQ1.services;
 
-public class ManufacturedTax {
+public class ManufacturedTax implements CalculateTaxService{
+
+    @Override
+    public double calculateTax(double price){
+        double tax = (price * 12.5) / 100;
+        return tax + 2 * tax / 100;
+    }
 }
--- Q1/src/assignmentQ1/services/Tax.java
+++ Q1/src/assignmentQ1/services/Tax.java
@@ -1,4 +1,11 @@
 package assignmentQ1.services;
 
 public class Tax {
+    public CalculateTaxService  getTax(int type) {
+    switch(type) {
+        case 1:
+            return 0;
+            case
+    }
+    }
 }
