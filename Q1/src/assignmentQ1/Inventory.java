64c247a0add99f70a818ff301b712a42e8496fef
Piyush Mehta <me@piyushmehta.com>
2020-01-18 16:27:32 +0530

    arraylist of item

--- Q1/src/assignmentQ1/Inventory.java
+++ Q1/src/assignmentQ1/Inventory.java
@@ -1,8 +1,9 @@
 package assignmentQ1;
 
 import java.util.ArrayList;
-
+import assignmentQ1.model.Item;
 public class Inventory {
+    private static ArrayList<Item> arrayListItems = new ArrayList<Item>();
 
 }
 
--- Q1/src/assignmentQ1/model/Item.java
+++ Q1/src/assignmentQ1/model/Item.java
@@ -1,4 +1,16 @@
 package assignmentQ1.model;
 
+// Properties of item
 public class Item {
+    public String name;
+    public double price;
+    public int quantiy;
+    public int type;
+
+    public Item(String name, double price, int quantiy, int type) {
+        this.name = name;
+        this.price = price;
+        this.quantiy = quantiy;
+        this.type = type;
+    }
 }
