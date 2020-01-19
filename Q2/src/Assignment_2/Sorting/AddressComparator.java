1874591086ebd7a1cdedb19e0d48db35fd023f22
Piyush Mehta <piyushmehta@Piyush-MBP.local>
2020-01-19 22:28:38 +0530

    AddressComparator

--- Q2/src/Assignment_2/Sorting/AddressComparator.java
+++ Q2/src/Assignment_2/Sorting/AddressComparator.java
@@ -1,4 +1,20 @@
 package Assignment_2.Sorting;
 
-public class AdressComparator {
+import Assignment_2.models.UserInfo;
+import java.util.Comparator;
+
+public class AddressComparator implements Comparator<UserInfo>{
+
+    public int compare(UserInfo user1, UserInfo user2){
+        if(user1.address.equals(user2.address)){
+            return 0;
+        }
+        else if(user1.address.compareTo(user2.address)>0){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
 }
