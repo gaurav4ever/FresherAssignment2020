e7e4c7ed293a99a1a2a6658e0ffc7619fb65b55c
Piyush Mehta <me@piyushmehta.com>
2020-01-20 09:31:12 +0530

    Q3 prettier

--- Q3/src/Node.java
+++ Q3/src/Node.java
@@ -5,22 +5,20 @@ public class Node {
     String name;
     HashMap<Integer, Node> children;
 
-    public Node(int id,String name)
-    {
+    public Node(int id, String name) {
         this.id = id;
         this.name = name;
         this.children = new HashMap<>();
     }
 
-    public void add(int id,String name,int[] children)
-    {
+    public void add(int id, String name, int[] children) {
         this.id = id;
         this.name = name;
     }
-    public void fetchNodeInfo(int id)
-    {
-        System.out.println("ID : "+id+" Name : "+name+" Children : ");
-        for(Integer i : children.keySet()){
+
+    public void fetchNodeInfo(int id) {
+        System.out.println("ID : " + id + " Name : " + name + " Children : ");
+        for (Integer i : children.keySet()) {
             System.out.println(i);
         }
 
