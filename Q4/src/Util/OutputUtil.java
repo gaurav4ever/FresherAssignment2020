494948040df1ddfbfca41d6cd919f42469257160
Piyush Mehta <me@piyushmehta.com>
2020-01-20 11:56:06 +0530

    output streaam

--- Q4/src/Util/OutputUtil.java
+++ Q4/src/Util/OutputUtil.java
@@ -1,4 +1,15 @@
 package Util;
 
+import model.Item;
+
 public class OutputUtil {
+    // output Util
+    public static void printOutput(Item productListItems, double salesTax, double totalPrice) {
+        System.out.println("Output details");
+        System.out.println("Product Name:\t" + productListItems.productName);
+        System.out.println("Product Price:\t" + productListItems.productPrice);
+        System.out.println("Product Name:\t" + salesTax);
+        System.out.println("Product Name:\t \n" + totalPrice);
+    }
 }
+
