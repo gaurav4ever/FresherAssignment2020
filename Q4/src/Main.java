42214a449b5cecd44d3f170b533150ecf82abd13
Piyush Mehta <me@piyushmehta.com>
2020-01-20 11:25:21 +0530

    fixing according to the table

--- Q4/src/Main.java
+++ Q4/src/Main.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Main {
+public class Main extends Thread{
     public static void main(String args[]){
         try{
 
@@ -15,9 +15,9 @@ public class Main {
             Statement stmt=con.createStatement();
 
 //step4 execute query
-            ResultSet rs=stmt.executeQuery("select * from Inventory");
+                ResultSet rs=stmt.executeQuery("select * from Inventory");
             while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+                System.out.println(rs.getString(1)+"  "+rs.getFloat(2)+"  "+rs.getInt(3) + " " +rs.getString(4));
 
 //step5 close the connection object
             con.close();
