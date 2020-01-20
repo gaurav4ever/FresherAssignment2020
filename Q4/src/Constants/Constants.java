92706cc102ad3f92638737d2efb3b074bd48c303
Piyush Mehta <me@piyushmehta.com>
2020-01-20 12:54:32 +0530

    Consts

---
+++ Q4/src/Constants/Constants.java
@@ -0,0 +1,8 @@
+package Constants;
+
+public class Constants {
+    public static final String DB_ADDRESS = "jdbc:mysql://localhost:3306/Q4";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "1234567890";
+    public static final String RESULT_QUERY = "select * from Inventory";
+}
--- Q4/src/Main.java
+++ Q4/src/Main.java
@@ -1,22 +1,34 @@
+import Util.InputUtil;
+import Util.OutputUtil;
+import model.Item;
+import Constants.Constants;
+import Services.CalculateTaxService;
+import Services.Tax;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main extends Thread{
+    private static ArrayList<Item> productList = new ArrayList<Item>();
     public static void main(String args[]){
+
         try{
+            productList = InputUtil.getInputItem();
+            calculateTax();
+
+
 
 //step2 create  the connection object
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/Q4","root","1234567890");
+                    Constants.DB_ADDRESS,Constants.USER_NAME,Constants.PASSWORD);
 
 //step3 create the statement object
             Statement stmt=con.createStatement();
 
-
 //step4 execute query
-                ResultSet rs=stmt.executeQuery("select * from Inventory");
+                ResultSet rs=stmt.executeQuery(Constants.RESULT_QUERY);
             while(rs.next())
                 System.out.println(rs.getString(1)+"  "+rs.getFloat(2)
                         +"  "+rs.getInt(3) + " " +rs.getString(4));
@@ -26,5 +38,17 @@ public class Main extends Thread{
 
         }catch(Exception e){ System.out.println(e);}
 
+    }
+    private static void calculateTax() {
+        for (Item arrayListItem : productList) {
+            double salesTax = getCalculatedTax(arrayListItem.productType, arrayListItem.productPrice);
+            double totalPrice = (arrayListItem.productPrice + salesTax) * arrayListItem.productQuantity;
+            OutputUtil.printOutput(arrayListItem, salesTax, totalPrice);
+        }
+    }
+
+    private static double getCalculatedTax(int type, double price){
+        CalculateTaxService calculateTaxService = new Tax().getTax(type);
+        return calculateTaxService.calculateTax(price);
     }
 }
