f97e0674820c4c9709192c85f440fc465d14e90d
Piyush Mehta <me@piyushmehta.com>
2020-01-22 17:40:31 +0530

    Subscription Controllerr

--- Q5/demo/src/main/java/com/example/demo/Controller/SubscriptionsController.java
+++ Q5/demo/src/main/java/com/example/demo/Controller/SubscriptionsController.java
@@ -1,4 +1,42 @@
 package com.example.demo.Controller;
 
+import java.util.List;
+
+import com.example.demo.Model.Subscription;
+import com.example.demo.Repositories.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class SubscriptionsController {
+
+    @Autowired
+    SubscriptionRepository subscriptionRepository;
+
+    @GetMapping("/subscriptions")
+    public List<Subscription> index() {
+        return subscriptionRepository.findAll();
+    }
+
+    @GetMapping("/subscriptions/{id}")
+    public Subscription show(@PathVariable String id) throws Exception{
+        int subscriptionId = Integer.parseInt(id);
+        return subscriptionRepository.findById(subscriptionId).orElseThrow(Exception::new);
+    }
+
+    @PostMapping("/subscriptions")
+    public Subscription create(@RequestBody Subscription subscription){
+//        String name = newsLetter.get("name");
+        return subscriptionRepository.save(subscription);
+    }
+
+    @DeleteMapping("subscriptions/{id}")
+    public String delete(@PathVariable String id){
+        int newsLetterId = Integer.parseInt(id);
+        subscriptionRepository.deleteById(newsLetterId);
+        return "Subscription Deleted";
+    }
+
+
+
 }
