b46649862befbb994eddcad2e4f56a62dfbdc748
Piyush Mehta <me@piyushmehta.com>
2020-01-22 17:41:19 +0530

    Sub repo

--- Q5/demo/src/main/java/com/example/demo/Repositories/SubscriptionRepository.java
+++ Q5/demo/src/main/java/com/example/demo/Repositories/SubscriptionRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.Repositories;
 
-public interface SubscriptionRepository {
+import com.example.demo.Model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
+//    List<NewsLetter> findByTitleContaingOrContentContaining(String text, String textAgain);
 }
